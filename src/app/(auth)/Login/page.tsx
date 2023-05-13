fe5085840753419285ad8562ab1f2b32e114572a
francisflow <Iamngigikaranja1@aol.com>
2023-05-13 12:49:58 +0300

    Created a default error boundary to handle errors

--- src/app/(auth)/Login/page.tsx
+++ src/app/(auth)/Login/page.tsx
@@ -5,15 +5,16 @@ import axios from "axios"
 const wait = (ms:number) => new Promise((resolve)=> setTimeout(resolve, ms))
  
 const loginPage = async () => {
-   
     await wait(3000)
     const {data} = await axios.get(
         "https://jsonplaceholder.typicode.com/posts/1"
     )
+   throw new Error(`Could not do it`)
 
-    return <div>
+    return (<div>
         {JSON.stringify(data)}
-    </div>
+    </div>)
+
 }
  
 export default loginPage;
---
+++ src/app/error.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { FC } from "react";
+interface errorProps {
+    
+}
+ 
+const error: FC<errorProps> = () => {
+    return (
+        <div>
+            Error has occurred
+        </div>
+      );
+}
+ 
+export default error;
